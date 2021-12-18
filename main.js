c1f165c38bd427a02fccb1e3c5c3c64cedcc759f
Jeremy Asingura <blessedjosh234@gmail.com>
2021-12-18 09:10:51 +0300

    randomized the text in the button

--- main.js
+++ main.js
@@ -8,10 +8,12 @@ button.addEventListener("click", function(){
      let colors = ["red", "blue", "green", "orange"];
      let colors2 = ["lightgoldenrodyellow", "lawngreen", "azure", "thistle"];
      let randomTexts = ["How are you?", "Jeremy is awesome.", "CodeImpact is Super fun.", "God Loves Me so much."]
+     let randomTexts2 = ["Feel me", "Touch Me", "Goats", "Jesus", "Tintin"];
      let headingElement = document.getElementById("header");
 
     // document.location.reload();
      headingElement.innerText = randomTexts[Math.floor(Math.random()*randomTexts.length)];
+     button.innerText = randomTexts2[Math.floor(Math.random()*randomTexts2.length)];
      headingElement.style.color = colors2[Math.floor(Math.random()*colors2.length)];
      bodyElement.style.backgroundColor = colors[Math.floor(Math.random()*colors.length)];
 })
